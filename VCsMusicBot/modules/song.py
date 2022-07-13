6999bd5ce97ad1b9968bf609dd11b9d111eb2577
AUGSTUN <97671641+AUGSTUN@users.noreply.github.com>
2022-07-14 00:04:28 +0530

    Update song.py

--- VCsMusicBot/modules/song.py
+++ VCsMusicBot/modules/song.py
@@ -247,7 +247,7 @@ def time_to_seconds(time):
     return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(":"))))
 
 
-@Client.on_message(filters.command("saavn") & ~filters.edited)
+@Client.on_message(filters.command("saavn"))
 async def jssong(_, message):
     global is_downloading
     if len(message.command) < 2:
@@ -286,7 +286,7 @@ async def jssong(_, message):
 # Deezer Music
 
 
-@Client.on_message(filters.command("deezer") & ~filters.edited)
+@Client.on_message(filters.command("deezer"))
 async def deezsong(_, message):
     global is_downloading
     if len(message.command) < 2:
