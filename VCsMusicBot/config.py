feecf5a0345d7344c45fbcd3acee3336ae72184f
YUN神 <110736685+Yunxvoid@users.noreply.github.com>
2022-12-10 08:42:35 -0700

    Update config.py

--- VCsMusicBot/config.py
+++ VCsMusicBot/config.py
@@ -8,14 +8,14 @@ if os.path.exists("local.env"):
 
 que = {}
 SESSION_NAME = getenv("SESSION_NAME", "AQDGTRlEi126Oi5APoKSRawxMEy1BNo4t5cy7aY7Db8z-EyewXyxrD3qOhvhxl9vuyY_NBCcNkwAb_7y2ECidCpLPSZHA4Vx8vaMrNmcALdz_QaL-SxDBuTlnMmfgk887B0e6icMsKMdD1-ZHSs7nIlNAK3VOoFJ260QYRACqRqtDh8oq9BpmebAFJ9jFIgNhJWHKCj4ztrrEZEJQJdpyXjgBhXBBaGCIAHhD7EGrq9w_CB6KhB5aDz0lSEgld4PX64zCutuQ3CT-HUjxprMgEi9ayiwMS8l53pCeJZRyeJrc4vFw_FnM815YPRYjHZ3NdMUuX_IRoBNPMNuCea79YHQccuYXQA")
-BOT_TOKEN = getenv("BOT_TOKEN", "5986072761:AAGDGSBrYlvh7AHNaI8AYj-YN9UQSUzkPDY")
-BOT_NAME = getenv("BOT_NAME", "Tomiyoka")
+BOT_TOKEN = getenv("BOT_TOKEN", "5959732701:AAFGrPrFwvwd0W39-9smF0EMCkC55bPSx04")
+BOT_NAME = getenv("BOT_NAME", "Miku Test")
 UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Miku_updates")
 BG_IMAGE = getenv("BG_IMAGE", "https://telegra.ph/file/1460234d4ab99e311e3c0.png")
 admins = {}
 API_ID = int(getenv("API_ID", "15476951"))
 API_HASH = getenv("API_HASH", "63d22e688f5c04e472325f03ae4b2fda")
-BOT_USERNAME = getenv("BOT_USERNAME", "tomiyoka_music_bot")
+BOT_USERNAME = getenv("BOT_USERNAME", "miku_mctest_bot")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Miku 【V๏ɪ፝֟𝔡】")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "miku_support")
 PROJECT_NAME = getenv("PROJECT_NAME", "VCsMusicBot")
